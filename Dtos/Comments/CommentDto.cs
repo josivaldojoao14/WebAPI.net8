c3b83451a8dba21edf498dfb1345e3b235926194
Josivaldo Joao <josivaldojoao14@gmail.com>
2024-02-06 23:39:32 -0300

    dto and mapper added

---
+++ Dtos/Comments/CommentDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet8_api.Dtos.Comments
+{
+    public class CommentDto
+    {
+        
+    }
+}
---
+++ Mappers/StockMappers.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet8_api.Dtos.Stock;
+using dotnet8_api.Models;
+
+namespace dotnet8_api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto 
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Industry = stockModel.Industry,
+                LastDiv = stockModel.LastDiv,
+                MarketCap = stockModel.MarketCap,
+                Purchase = stockModel.Purchase
+            };
+        }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto stockModel)
+        {
+            return new Stock
+            {
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Industry = stockModel.Industry,
+                LastDiv = stockModel.LastDiv,
+                MarketCap = stockModel.MarketCap,
+                Purchase = stockModel.Purchase
+            };
+        }
+    }
+}
